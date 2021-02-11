f7ee54d733163bddef49b089ed42ab59dfcc56e3
adder46 <dedmauz69@gmail.com>
2021-02-11 10:47:22 +0100

    Aesthetics

--- src/lib.rs
+++ src/lib.rs
@@ -12,21 +12,21 @@ impl RippleCarryAdder {
     pub fn add(&mut self, a: u8, b: u8) -> Result<u8, &'static str> {
         for i in 0..self.adders.len() {
             /* We are extracting the bits in reverse, by shifting right `i` positions,
-             *  where 0 <= i < 8. In the beginning, i=0, so we are not shifting anything.
-             *  However, each time through the loop, the number of positions (`i`) we are
-             *  shifting right increases by 1 until it reaches 7. This means that the
-             *  previously extracted bit is discarded, and the bit we want to extract next
-             *  comes to its place, i.e., to the least significant position. By Boolean ANDing
-             *  the resulting number with 1 (mask), the individual bit is effectively extracted
-             *  from the number - masking out the rest.
+             * where 0 <= i < 8. In the beginning, i=0, so we are not shifting anything.
+             * However, each time through the loop, the number of positions (`i`) we are
+             * shifting right increases by 1 until it reaches 7. This means that the
+             * previously extracted bit is discarded, and the bit we want to extract next
+             * comes to its place, i.e., to the least significant position. By Boolean ANDing
+             * the resulting number with 1 (mask), the individual bit is effectively extracted
+             * from the number - masking out the rest.
              */
 
             let bit1 = Bit((a >> i) & 1);
             let bit2 = Bit((b >> i) & 1);
 
             /* The adders are split in half at the position following the current adder,
-             *  which means that the current adder becomes the last one in the left half,
-             *  and the next adder is the first one in the right half.
+             * which means that the current adder becomes the last one in the left half,
+             * and the next adder is the first one in the right half.
              */
 
             let (left, right) = self.adders.split_at_mut(i + 1);
@@ -40,9 +40,9 @@ impl RippleCarryAdder {
                 None => {
                     if current_adder.carry_out == Bit(1) {
                         /* having no adders that come after the current one,
-                         *  means that current adder is the last one, and if
-                         *  the carry out for that adder is 1, 8-bit ripple carry
-                         *  adder overflowed.
+                         * means that current adder is the last one, and if
+                         * the carry out for that adder is 1, 8-bit ripple carry
+                         * adder overflowed.
                          */
                         return Err("Overflow.");
                     }
@@ -54,11 +54,11 @@ impl RippleCarryAdder {
 
     fn get_result(&self) -> u8 {
         /* Since the adding process was in reverse,
-         *  when building the result, the adders are
-         *  reversed so that the adder holding the MSB
-         *  comes first. We start from 0, and pack the bits
-         *  back into the result, by shifting left by 1
-         *  each time through the loop and ORing with the sum.
+         * when building the result, the adders are
+         * reversed so that the adder holding the MSB
+         * comes first. We start from 0, and pack the bits
+         * back into the result, by shifting left by 1
+         * each time through the loop and ORing with the sum.
          */
         let mut result = 0;
         for adder in self.adders.iter().rev() {
@@ -158,17 +158,17 @@ mod tests {
 
     #[apply(possible_combinations)]
     fn full_add(a: Bit, b: Bit, expected_carry_out: Bit, expected_sum: Bit) {
-        let mut fulladder: FullAdder = Default::default();
-        fulladder.add(a, b);
-        assert_eq!(fulladder.carry_out, expected_carry_out);
-        assert_eq!(fulladder.sum, expected_sum);
+        let mut full_adder: FullAdder = Default::default();
+        full_adder.add(a, b);
+        assert_eq!(full_adder.carry_out, expected_carry_out);
+        assert_eq!(full_adder.sum, expected_sum);
     }
 
     #[apply(possible_combinations)]
     fn half_add(a: Bit, b: Bit, expected_carry_out: Bit, expected_sum: Bit) {
-        let mut halfadder: HalfAdder = Default::default();
-        halfadder.add(a, b);
-        assert_eq!(halfadder.carry_out, expected_carry_out);
-        assert_eq!(halfadder.sum, expected_sum);
+        let mut half_adder: HalfAdder = Default::default();
+        half_adder.add(a, b);
+        assert_eq!(half_adder.carry_out, expected_carry_out);
+        assert_eq!(half_adder.sum, expected_sum);
     }
 }
