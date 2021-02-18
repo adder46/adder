def91e8736c76a9b8b1545af28087eb7ea6fb828
adder46 <dedmauz69@gmail.com>
2021-02-18 01:25:00 +0100

    Improve comments

--- src/lib.rs
+++ src/lib.rs
@@ -11,38 +11,33 @@ pub struct RippleCarryAdder {
 impl RippleCarryAdder {
     pub fn add(&mut self, a: u8, b: u8) -> Result<u8, &'static str> {
         for i in 0..self.adders.len() {
-            /* We are extracting the bits in reverse, by shifting right `i` positions,
-             * where 0 <= i < 8. In the beginning, i=0, so we are not shifting anything.
-             * However, each time through the loop, the number of positions (`i`) we are
-             * shifting right increases by 1 until it reaches 7. This means that the
-             * previously extracted bit is discarded, and the bit we want to extract next
-             * comes to its place, i.e., to the least significant position. By Boolean ANDing
-             * the resulting number with 1 (mask), the individual bit is effectively extracted
-             * from the number - masking out the rest.
+            /* The bits are extracted from the narrow end by shifting right `i` positions,
+             * where 0 <= i < 8. On first iteration, i == 0, meaning that nothing is shifted.
+             * However, each time through the loop, the number of shifted positions (`i`)
+             * increases by 1 until it reaches 7. As a result, previously extracted bit is
+             * discarded and the bit we want to extract next comes to its place, i.e., becomes LSB.
+             * By Boolean ANDing the resulting number with 1 (mask), the individual bit at the end
+             * is effectively extracted from the number - masking out the rest.
              */
-
             let bit1 = Bit((a >> i) & 1);
             let bit2 = Bit((b >> i) & 1);
-
-            /* The adders are split in half at the position following the current adder,
-             * which means that the current adder becomes the last one in the left half,
-             * and the next adder is the first one in the right half.
+            /* The adders are split into two halves at the position following the current adder (`i`),
+             * which places the current adder in the last position in the left half, while the next
+             * adder becomes the first one in the right half.
              */
-
             let (left, right) = self.adders.split_at_mut(i + 1);
             let current_adder = &mut left[i];
             current_adder.add(bit1, bit2);
             match right.get_mut(0) {
                 Some(next_adder) => {
-                    // pass the carry bit if there is an adder following the current one
+                    // Pass the carry bit to the next adder if the current adder is not the last one.
                     next_adder.carry_in = current_adder.carry_out;
                 }
                 None => {
                     if current_adder.carry_out == Bit(1) {
-                        /* having no adders that come after the current one,
-                         * means that current adder is the last one, and if
-                         * the carry out for that adder is 1, 8-bit ripple carry
-                         * adder overflowed.
+                        /* Having no adders that come after the current one,
+                         * means that current adder is the last one. Furthermore,
+                         * if the carry out for that adder is 1, we have an overflow.
                          */
                         return Err("Overflow.");
                     }
@@ -53,12 +48,11 @@ impl RippleCarryAdder {
     }
 
     fn get_result(&self) -> u8 {
-        /* Since the adding process was in reverse,
-         * when building the result, the adders are
-         * reversed so that the adder holding the MSB
-         * comes first. We start from 0, and pack the bits
-         * back into the result, by shifting left by 1
-         * each time through the loop and ORing with the sum.
+        /* Since the addition was done starting from the narrow end,
+         * when building the result, the adders are reversed so that
+         * the adder holding the MSB comes first. The bits are packed
+         * back into the result by shifting left by 1 each time through
+         * the loop and ORing the result with the sum of an individual adder.
          */
         let mut result = 0;
         for adder in self.adders.iter().rev() {
